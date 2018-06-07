2d5e3421a3a700917043d1b9cd259e7e535a885c
Eric Auger <eric.auger@redhat.com>
2018-06-07 12:29:26 -0400

    kvm: add kvm_arm_get_max_vm_phys_shift
    Add the kvm_arm_get_max_vm_phys_shift() helper that returns the
    log of the maximum IPA size supported by KVM. This capability
    needs to be known to create the VM with a specific IPA max size
    (kvm_type passed along KVM_CREATE_VM ioctl.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    ---
    v4 -> v5:
    - return 40 if the host does not support the capability
    
    v3 -> v4:
    - s/s/ms in kvm_arm_get_max_vm_phys_shift function comment
    - check KVM_CAP_ARM_VM_IPA_SIZE extension
    
    v1 -> v2:
    - put this in ARM specific code

--- target/arm/kvm_arm.h
+++ target/arm/kvm_arm.h
@@ -208,6 +208,14 @@ bool kvm_arm_get_host_cpu_features(ARMHostCPUFeatures *ahcf);
 void kvm_arm_set_cpu_features_from_host(ARMCPU *cpu);
 
 /**
+ * kvm_arm_get_max_vm_phys_shift - Returns log2 of the max IPA size
+ * supported by KVM
+ *
+ * @ms: Machine state handle
+ */
+int kvm_arm_get_max_vm_phys_shift(MachineState *ms);
+
+/**
  * kvm_arm_sync_mpstate_to_kvm
  * @cpu: ARMCPU
  *
@@ -237,6 +245,11 @@ static inline void kvm_arm_set_cpu_features_from_host(ARMCPU *cpu)
      */
     cpu->kvm_target = QEMU_KVM_ARM_TARGET_NONE;
     cpu->host_cpu_probe_failed = true;
+}
+
+static inline int kvm_arm_get_max_vm_phys_shift(MachineState *ms)
+{
+    return -ENOENT;
 }
 
 static inline int kvm_arm_vgic_probe(void)
