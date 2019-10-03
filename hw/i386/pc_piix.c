12ddd20d107a44f8157753d8421b6c555a177f53
Kwangwoo Lee <kwangwoo.lee@sk.com>
2019-10-03 12:52:05 +0100

    nvdimm: Use configurable ACPI IO base and size
    This patch makes IO base and size configurable to create NPIO AML for
    ACPI NFIT. Since a different architecture like AArch64 does not use
    port-mapped IO, a configurable IO base is required to create correct
    mapping of ACPI IO address and size.
    
    Signed-off-by: Kwangwoo Lee <kwangwoo.lee@sk.com>
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -58,6 +58,7 @@
 #include "migration/misc.h"
 #include "kvm_i386.h"
 #include "sysemu/numa.h"
+#include "hw/i386/acpi-build.h"
 
 #define MAX_IDE_BUS 2
 
@@ -303,6 +304,7 @@ else {
 
     if (machine->nvdimms_state->is_enabled) {
         nvdimm_init_acpi_state(machine->nvdimms_state, system_io,
+                               x86_nvdimm_acpi_dsmio,
                                pcms->fw_cfg, OBJECT(pcms));
     }
 }
