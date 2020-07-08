63693b0bc358ee8b3d96882a74860909f9290849
Ben Widawsky <ben.widawsky@intel.com>
2020-07-08 12:41:10 -0700

    acpi/cxl: Create the CEDT (9.14.1)
    The CXL Early Discovery Table is defined in the CXL 2.0 specification as
    a way for the OS to get CXL specific information from the system
    firmware.
    
    CXL 2.0 specification adds an _HID, ACPI0016, for CXL capable host
    bridges, with a _CID of PNP0A08 (PCIe host bridge). CXL aware software
    is able to use this initiate the proper _OSC method, and get the _UID
    which is referenced by the CEDT. Therefore the existence of an ACPI0016
    device allows a CXL aware driver perform the necessary actions. For a
    CXL capable OS, this works. For a CXL unaware OS, this works.
    
    CEDT awaremess requires more. The motivation for ACPI0017 is to provide
    the possibility of having a Linux CXL module that can work on a legacy
    Linux kernel. Linux core PCI/ACPI which won't be built as a module,
    will see the _CID of PNP0A08 and bind a driver to it. If we later loaded
    a driver for ACPI0016, Linux won't be able to bind it to the hardware
    because it has already bound the PNP0A08 driver. The ACPI0017 device is
    an opportunity to have an object to bind a driver will be used by a
    Linux driver to walk the CXL topology and do everything that we would
    have preferred to do with ACPI0016.
    
    There is another motivation for an ACPI0017 device which isn't
    implemented here. An operating system needs an attach point for a
    non-volatile region provider that understands cross-hostbridge
    interleaving. Since QEMU emulation doesn't support interleaving yet,
    this is more important on the OS side, for now.
    
    As of CXL 2.0 spec, only 1 sub structure is defined, the CXL Host Bridge
    Structure (CHBS) which is primarily useful for telling the OS exactly
    where the MMIO for the host bridge is.
    
    v2: Update CHBS to spec released definition
    v3: squash ACPI0017 in now that it's ratified.
    
    Link: https://lore.kernel.org/linux-cxl/20210115034911.nkgpzc756d6qmjpl@intel.com/T/#t
    Signed-off-by: Ben Widawsky <ben.widawsky@intel.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- hw/i386/acpi-build.c
+++ hw/i386/acpi-build.c
@@ -76,6 +76,8 @@
 #include "hw/acpi/hmat.h"
 #include "hw/acpi/viot.h"
 
+#include "hw/acpi/cxl.h"
+
 /* These are used to size the ACPI tables for -M pc-i440fx-1.7 and
  * -M pc-i440fx-2.0.  Even if the actual amount of AML generated grows
  * a little bit, there should be plenty of free space since the DSDT
@@ -1425,6 +1427,19 @@ static void init_pci_acpi(Aml *dev, int uid, int type, bool native_pcie_hp )
     }
 }
 
+static void build_acpi0017(Aml *table)
+{
+    Aml *dev;
+    Aml *scope;
+
+    scope =  aml_scope("_SB");
+    dev = aml_device("CXLM");
+    aml_append(dev, aml_name_decl("_HID", aml_string("ACPI0017")));
+
+    aml_append(scope, dev);
+    aml_append(table, scope);
+}
+
 static void
 build_dsdt(GArray *table_data, BIOSLinker *linker,
            AcpiPmInfo *pm, AcpiMiscInfo *misc,
@@ -1444,6 +1459,7 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
 #ifdef CONFIG_TPM
     TPMIf *tpm = tpm_find();
 #endif
+    bool cxl_present = false;
     int i;
     VMBusBridge *vmbus_bridge = vmbus_bridge_find();
     AcpiTable table = { .sig = "DSDT", .rev = 1, .oem_id = x86ms->oem_id,
@@ -1590,7 +1606,7 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
 
             scope = aml_scope("\\_SB");
             if (type == CXL) {
-                dev = aml_device("CXL%.01X", pci_bus_uid(bus));
+                dev = aml_device("CXL%.01X", uid);
             } else {
                 dev = aml_device("PC%.02X", bus_num);
             }
@@ -1611,11 +1627,16 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
 
             /* Handle the ranges for the PXB expanders */
             if (type == CXL) {
+                cxl_present = true;
                 uint64_t base = CXL_HOST_BASE + uid * 0x10000;
                 crs_range_insert(crs_range_set.mem_ranges, base,
                                  base + 0x10000 - 1);
             }
         }
+    }
+
+    if (cxl_present) {
+        build_acpi0017(dsdt);
     }
 
     /*
@@ -2661,6 +2682,9 @@ void acpi_build(AcpiBuildTables *tables, MachineState *machine)
                           machine->nvdimms_state, machine->ram_slots,
                           x86ms->oem_id, x86ms->oem_table_id);
     }
+
+    cxl_build_cedt(table_offsets, tables_blob, tables->linker,
+                   x86ms->oem_id, x86ms->oem_table_id);
 
     acpi_add_table(table_offsets, tables_blob);
     build_waet(tables_blob, tables->linker, x86ms->oem_id, x86ms->oem_table_id);
