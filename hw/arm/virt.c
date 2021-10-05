5739a8c1339466595f200bb87ac0e447d94bbf8d
Eric Auger <eric.auger@redhat.com>
2021-10-05 10:53:13 +0200

    hw/arm/virt-acpi-build: Add IORT RMR regions to handle MSI nested binding
    To handle SMMUv3 nested stage support it is practical to
    expose the guest with reserved memory regions (RMRs)
    covering the IOVAs used by the host kernel to map
    physical MSI doorbells.
    
    Those IOVAs belong to [0x8000000, 0x8100000] matching
    MSI_IOVA_BASE and MSI_IOVA_LENGTH definitions in kernel
    arm-smmu-v3 driver. This is the window used to allocate
    IOVAs matching physical MSI doorbells.
    
    With those RMRs, the guest is forced to use a flat mapping
    for this range. Hence the assigned device is programmed
    with one IOVA from this range. Stage 1, owned by the guest
    has a flat mapping for this IOVA. Stage2, owned by the VMM
    then enforces a mapping from this IOVA to the physical
    MSI doorbell.
    
    The creation of those RMR nodes only is relevant if nested
    stage SMMU is in use, along with VFIO. As VFIO devices can be
    hotplugged, all RMRs need to be created in advance. Hence
    the patch introduces a new arm virt "nested-smmuv3" iommu type.
    
    ARM DEN 0049E.b IORT specification also mandates that when
    RMRs are present, the OS must preserve PCIe configuration
    performed by the boot FW. So along with the RMR IORT nodes,
    a _DSM function #5, as defined by PCI FIRMWARE SPECIFICATION
    EVISION 3.3, chapter 4.6.5 is added to PCIe host bridge
    and PCIe expander bridge objects.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Suggested-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    
    ---
    
    v2 -> v3:
    - Comply with IORT E.d spec. RMR node rev = 3; IORT rev = 5
      With this spec revision, the restriction on number of
      Stream IDs that can be associated with memory ranges in an
      RMR node was removed. So no need anymore to define 1 RMR node per
      SID!

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -1320,7 +1320,7 @@ static void create_smmu(const VirtMachineState *vms,
     DeviceState *dev;
     MachineState *ms = MACHINE(vms);
 
-    if (vms->iommu != VIRT_IOMMU_SMMUV3 || !vms->iommu_phandle) {
+    if (!virt_has_smmuv3(vms) || !vms->iommu_phandle) {
         return;
     }
 
@@ -1504,6 +1504,7 @@ static void create_pcie(VirtMachineState *vms)
 
         switch (vms->iommu) {
         case VIRT_IOMMU_SMMUV3:
+        case VIRT_IOMMU_NESTED_SMMUV3:
             create_smmu(vms, vms->bus);
             qemu_fdt_setprop_cells(ms->fdt, nodename, "iommu-map",
                                    0x0, vms->iommu_phandle, 0x0, 0x10000);
@@ -2574,6 +2575,8 @@ static char *virt_get_iommu(Object *obj, Error **errp)
         return g_strdup("none");
     case VIRT_IOMMU_SMMUV3:
         return g_strdup("smmuv3");
+    case VIRT_IOMMU_NESTED_SMMUV3:
+        return g_strdup("nested-smmuv3");
     default:
         g_assert_not_reached();
     }
@@ -2585,6 +2588,8 @@ static void virt_set_iommu(Object *obj, const char *value, Error **errp)
 
     if (!strcmp(value, "smmuv3")) {
         vms->iommu = VIRT_IOMMU_SMMUV3;
+    } else if (!strcmp(value, "nested-smmuv3")) {
+        vms->iommu = VIRT_IOMMU_NESTED_SMMUV3;
     } else if (!strcmp(value, "none")) {
         vms->iommu = VIRT_IOMMU_NONE;
     } else {
