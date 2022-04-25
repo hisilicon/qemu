9b8e94a9af01dc8d34b754f7da21402031fcf9ee
Salil Mehta <salil.mehta@huawei.com>
2022-04-25 17:40:46 +0100

    arm/cpuhp: Add common cpu utility for possible vcpus
    Adds various utility functions which might be required to fetch or check the
    state of the possible vcpus. This also introduces concept of *disabled* vcpus,
    which are part of the *possible* vcpus but are not part of the *present* vcpu.
    This state shall be used during machine init time to check the presence of
    vcpus.
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Signed-off-by: Jonathan Cameron <jonathan.cameron@huawei.com>

--- include/hw/core/cpu.h
+++ include/hw/core/cpu.h
@@ -402,6 +402,7 @@ struct CPUState {
     SavedIOTLB saved_iotlb;
 #endif
 
+    bool disabled;
     /* TODO Move common fields from CPUArchState here. */
     int cpu_index;
     int cluster_index;
@@ -762,6 +763,26 @@ static inline bool cpu_in_exclusive_context(const CPUState *cpu)
  * Returns: The CPU or %NULL if there is no matching CPU.
  */
 CPUState *qemu_get_cpu(int index);
+
+/**
+ * qemu_get_possible_cpu:
+ * @index: The CPUState@cpu_index value of the CPU to obtain.
+ *
+ * Gets a CPU matching @index.
+ *
+ * Returns: The possible CPU or %NULL if there is no matching CPU.
+ */
+CPUState *qemu_get_possible_cpu(int index);
+
+/**
+ * qemu_present_cpu:
+ * @cpu: The vCPU to check
+ *
+ * Checks if the vcpu is amongst the present possible vcpus.
+ *
+ * Returns: True if it is present possible vcpu else false
+ */
+bool qemu_present_cpu(CPUState *cpu);
 
 /**
  * cpu_exists:
