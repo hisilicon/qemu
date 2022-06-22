714f00a58023c795c08dbb7264a8d7e6129d00c4
Nicolin Chen <nicolinc@nvidia.com>
2022-06-22 01:30:39 -0700

    hw/arm/smmu-common: Add a nested flag to SMMUState
    Add a nested flag in the SMMUState, passed in from device property.
    
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>

--- hw/arm/smmu-common.c
+++ hw/arm/smmu-common.c
@@ -683,6 +683,7 @@ static Property smmu_dev_properties[] = {
     DEFINE_PROP_UINT8("bus_num", SMMUState, bus_num, 0),
     DEFINE_PROP_LINK("primary-bus", SMMUState, primary_bus,
                      TYPE_PCI_BUS, PCIBus *),
+    DEFINE_PROP_BOOL("nested", SMMUState, nested, false),
     DEFINE_PROP_END_OF_LIST(),
 };
 
