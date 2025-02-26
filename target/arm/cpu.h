7666e15dfb8dabe14432a0c0fbd4e249a0185c9e
Eric Auger <eric.auger@redhat.com>
2025-02-26 13:26:38 +0100

    arm/cpu: Store aa64smfr0 into the idregs array
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Sebastian Ott <sebott@redhat.com>
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -1097,7 +1097,6 @@ struct ArchCPU {
         uint32_t dbgdidr;
         uint32_t dbgdevid;
         uint32_t dbgdevid1;
-        uint64_t id_aa64smfr0;
         uint64_t reset_pmcr_el0;
         uint64_t idregs[NUM_ID_IDX];
     } isar;
