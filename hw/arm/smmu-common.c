fa7c269577a8528fa487512b1cf3c85593cf5901
Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
2025-07-03 16:19:31 +0100

    hw/arm/smmu-common: Add accel property for SMMU dev
    Now user can set "accel=on". Have fun!
    
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>

--- hw/arm/smmu-common.c
+++ hw/arm/smmu-common.c
@@ -1022,6 +1022,7 @@ static const Property smmu_dev_properties[] = {
     DEFINE_PROP_BOOL("smmu_per_bus", SMMUState, smmu_per_bus, false),
     DEFINE_PROP_LINK("primary-bus", SMMUState, primary_bus,
                      TYPE_PCI_BUS, PCIBus *),
+    DEFINE_PROP_BOOL("accel", SMMUState, accel, false),
 };
 
 static void smmu_base_class_init(ObjectClass *klass, const void *data)
